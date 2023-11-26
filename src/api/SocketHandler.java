ca1dee09ba64df04ba2d97e667a652b311c35b17
hfjxjjd123 <licbusiness21@gmail.com>
2023-11-26 14:00:54 +0900

    주석추가

--- src/api/SocketHandler.java
+++ src/api/SocketHandler.java
@@ -8,12 +8,25 @@ public class SocketHandler {
         String cmd = TokenDecoder.parseCmd(data);
         List<String> tokens = TokenDecoder.parseToToken(data);
 
+        //TODO 핸들링 코드 작성
+        // System.out 코드들 'HANDLER(tokens)'로 대체할 것
         switch (cmd) {
             case "PSR":
+                //tokens = []
                 System.out.println("PSR");
                 break;
-            case "UMG":
-                //
+            case "RSR":
+                //tokens = []
+                System.out.println("RSR");
+                break;
+            case "ULM":
+                //tokens = m5,5/  r0,0/  b1,2/  h2,3/  t4,0/  t3,3/
+                System.out.println("ULM");
+                System.out.println(tokens);
+                break;
+            case "UDM":
+                //tokens = ["h4,2/"]
+                System.out.println("UDM");
                 break;
             default:
                 //invalid handling
