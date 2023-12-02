557708eda79220add887ff8177ddf962f4c1d7db
SpoofUoS <spoof_uos@icloud.com>
2023-12-02 17:27:03 +0900

    fix

--- src/api/AddressInfo.java
+++ src/api/AddressInfo.java
@@ -1,2 +1,19 @@
-package api;public class AddressInfo {
+package api;
+
+public class AddressInfo {
+    private String ipAddress;
+    private int port;
+
+    public AddressInfo(String ipAddress, int port) {
+        this.ipAddress = ipAddress;
+        this.port = port;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public int getPort() {
+        return port;
+    }
 }
