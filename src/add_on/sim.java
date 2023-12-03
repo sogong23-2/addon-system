255c3934d3e7e4c1265ddc3bba9a98610270671c
SpoofUoS <spoof_uos@icloud.com>
2023-12-03 20:58:56 +0900

    add handling wrong movement

--- src/add_on/sim.java
+++ src/add_on/sim.java
@@ -8,8 +8,12 @@ public class sim {
     private sensor hazard, colorblob, positioning;
     private movement move;
     private int [] position, direction; // 0: x, 1: y
+    private int r, c;
+    private boolean flag = false;
 
     public sim (int x, int y, int r, int c) {
+        this.r = r;
+        this.c = c;
         hazard = new hazard(r, c);
         colorblob = new colorblob(r, c);
         positioning = new positioning(r, c);
@@ -27,7 +31,24 @@ public class sim {
     }
 
     public void forward(){
-        this.position = move.forward(this.position, this.direction);
+        if (!this.flag) {
+            this.position = move.forward(this.position, this.direction);
+
+            Random rand = new Random();
+            int randomNum = rand.nextInt((100 - 1) + 1) + 1;
+            if (randomNum <= 100) {
+                int t = hazard.getSensorValue(this.position, this.direction)[0];
+                int[] tmp = move.forward(this.position, this.direction);
+                if (tmp[0] >= 0 && tmp[0] < this.r && tmp[1] >= 0 && tmp[1] < this.c && t != 1) {
+                    this.position = move.forward(this.position, this.direction);
+                    this.flag = true;
+                }
+            }
+        }
+        else {
+            this.flag = false;
+            this.position = move.forward(this.position, this.direction);
+        }
     }
 
     public void turn(){
